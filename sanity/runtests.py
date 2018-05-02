b60cf27ae93ea1257099d30211f3e55c29b6c53f
root <root@sturi-test-dist-git.localdomain>
2018-05-02 11:15:12 +0000

    Tests refactored

--- sanity/runtests.py
+++ sanity/runtests.py
@@ -2,33 +2,12 @@ import unittest
 
 import os
 import subprocess
-import xmlrunner
-
-from xml.dom import minidom
 
 
 def call_command(command_to_call):
     call = subprocess.check_call(command_to_call, shell=True)
     output = subprocess.check_output(command_to_call, shell=True)
     return call, output[:-1]
-
-
-def check_results(test_results_file):
-    xmldoc = minidom.parse(test_results_file)
-    itemlist = xmldoc.getElementsByTagName('testsuite')
-    failures = int(itemlist[0].attributes['failures'].value)
-    errors = int(itemlist[0].attributes['errors'].value)
-    if failures > 0 or errors > 0:
-        exit(1)
-    exit(0)
-
-
-def create_test_results_dir(results_dir='artifacts'):
-    cwd = os.path.dirname(os.getcwd())
-    full_patch = '{}/{}/'.format(cwd, results_dir)
-    if not os.path.exists(full_patch):
-        os.makedirs(full_patch)
-    return full_patch
 
 
 class TestBC(unittest.TestCase):
@@ -56,7 +35,7 @@ class TestBC(unittest.TestCase):
     def test_scale(self):
         result = call_command("echo 'scale = 2; 2 / 3' | bc")
         self.assertEqual( result[0], 0)
-        self.assertEqual( result[1], 'ss.66')
+        self.assertEqual( result[1], '.66')
 
     def test_remainder(self):
         result = call_command("echo '6 % 4' | bc")
@@ -70,11 +49,5 @@ class TestBC(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    results_dir = create_test_results_dir()
-    test_results_file = 'results.xml'
-    test_results_patch = results_dir + test_results_file
-    print("TRP", test_results_patch)
-    with open(test_results_patch, 'wb') as output:
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output),
-                      failfast=False, buffer=False, catchbreak=False)
-    check_results(test_results_patch)
+    unittest.main()
+
